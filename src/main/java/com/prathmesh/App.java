2f34987f27b2b3b31d10850b759a472d11b36a92
prathmeshrajmane <prathmesh.rajmane1@gmail.com>
2021-09-07 18:33:44 +0530

    first commit

---
+++ src/main/java/com/prathmesh/App.java
@@ -0,0 +1,13 @@
+package com.prathmesh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
